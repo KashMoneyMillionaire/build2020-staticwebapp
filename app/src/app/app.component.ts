fc56d1525f724046024ad908af24ff3a25b95693
Kash Cummings <kashleec@gmail.com>
2020-07-10 10:20:49 -0400

    add another route

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -8,3 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 }
+
+@Component({
+  selector: 'app-test',
+  template: 'Here we are in the test',
+  styleUrls: ['./app.component.scss']
+})
+export class TestComponent {
+}
