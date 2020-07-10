8ee773cf87c6556a7c577387c51b3c057c5ab175
Kash Cummings <kashleec@gmail.com>
2020-07-10 10:49:40 -0400

    add api

---
+++ api/GetTime.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Build2020.Demo
+{
+    public static class GetTime
+    {
+        [FunctionName("GetTime")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult(DateTime.Now);
+        }
+    }
+}
